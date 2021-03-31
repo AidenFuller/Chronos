972cf458f67cf42aea144ca38d8775c03d8a667b
TheCosmicNomad <aidenfuller314@gmail.com>
2021-03-31 20:29:28 +1100

    [CHR-11] - Created initial commit for sprint

--- Chronos.Client/Program.cs
+++ Chronos.Client/Program.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Chronos.Client
