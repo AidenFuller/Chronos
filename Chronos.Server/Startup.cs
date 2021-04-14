3504097b93a872f62a111e781716b437603cfd9e
TheCosmicNomad <aidenfuller314@gmail.com>
2021-04-14 20:57:58 +1000

    [CHR-17] Added gitignore exception for .db files

--- Chronos.Server/Startup.cs
+++ Chronos.Server/Startup.cs
@@ -12,7 +12,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Data.Sqlite;
 
 namespace Chronos.Server
 {
