6eed708929fd88dec7cee1bc3cdf7cc94a24217d
TheCosmicNomad <aidenfuller314@gmail.com>
2021-04-21 21:23:51 +1000

    [CHR-29] Added GetDegreeCourses API Controller

---
+++ Chronos.Server/Controllers/DegreeCoursesController.cs
@@ -0,0 +1,26 @@
+﻿using Chronos.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chronos.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DegreeCoursesController : ControllerBase
+    {
+        public IEnumerable<Course> Get(int degreeID)
+        {
+            using var db = new AppDbContext();
+            IEnumerable<int> courseIDs = db.CoreCourses.Where(i => i.DegreeID == degreeID).Select(i => i.CourseID);
+            
+            return
+                from course in db.Courses
+                join id in courseIDs on course.CourseID equals id
+                select course;
+        }
+    }
+}
