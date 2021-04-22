fd352ceae24cfaaa6aa3de3bc3c0f9a7e877c6fb
blobbyguy <sam.gillard44@gmail.com>
2021-04-22 19:37:23 +1000

    [CHR-33] Added MajorCoursesController

--- Chronos.Server/Controllers/MajorCoursesController.cs
+++ Chronos.Server/Controllers/MajorCoursesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Chronos.Shared.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +12,19 @@ namespace Chronos.Server.Controllers
     [ApiController]
     public class MajorCoursesController : ControllerBase
     {
+        public IEnumerable<Course> Get(int MajorID)
+        {
+            //Instance of DB
+            using var db = new AppDbContext();
+
+            //Select all courses where the MajorID is the given ID and return their course IDs
+            IEnumerable<int> CourseIDs = db.MajorCourses.Where(i => i.MajorID == MajorID).Select(i => i.CourseID);
+
+            //Grabs all courses from the course table  where the ID is in CourseIDs
+            return
+                from course in db.Courses
+                join id in CourseIDs on course.CourseID equals id
+                select course;
+        }
     }
 }
