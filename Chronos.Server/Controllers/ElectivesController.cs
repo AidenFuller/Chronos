33912b77a380015be45c77812e504a70bef8abd2
blobbyguy <sam.gillard44@gmail.com>
2021-04-22 20:39:13 +1000

    [CHR-34] Added ElectiveesController

---
+++ Chronos.Server/Controllers/ElectivesController.cs
@@ -0,0 +1,36 @@
+﻿using Chronos.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chronos.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ElectivesController : ControllerBase
+    {
+
+        public IEnumerable<Course> Get(int DegreeID, int MajorID)
+        {
+
+            //Instance of DB
+            using var db = new AppDbContext();
+
+            //Select all courseID where it is NOT a core course for the given Degree ID
+            IEnumerable<int> NotCoreCourseIDs = db.CoreCourses.Where(i => i.DegreeID != DegreeID).Select(i => i.CourseID);
+            //Select all courses where it is NOT a  major core course for the given Major ID
+            IEnumerable<int> NotMajorCourseIDs = db.MajorCourses.Where(i => i.MajorID != MajorID).Select(i => i.CourseID);
+
+
+            //Return all courses that are in either of the lists.
+            return
+                from course in db.Courses
+                join corecourseID in NotCoreCourseIDs on course.CourseID !equals corecourseID
+                join majorcourseID in NotMajorCourseIDs on course.CourseID !equals majorcourseID
+                select course;
+        }
+    }
+}
