5c17ed540ceb00a4016ab794b60ee567295d41c1
TheCurlyDude <jdawg093@gmail.com>
2021-04-22 20:14:48 +1000

    [CHR-30] Created the GetCourseController.cs... hopefully its good.

---
+++ Chronos.Server/Controllers/GetCourseController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Chronos.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chronos.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetCourseController : ControllerBase
+    {
+        [HttpGet]
+        public Course Get(int courseID)
+        {
+            using var db = new AppDbContext();
+            Course SelectedCourse = db.Courses.First(i => i.CourseID == courseID);
+
+            return SelectedCourse;
+        }
+    }
+}
