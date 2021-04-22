a9916d705c663bcb77ac05be33f0cdab81872392
TheCurlyDude <jdawg093@gmail.com>
2021-04-22 19:51:50 +1000

    [CHR-28] Created the GetDegreeController.cs. This should include all of the required API data.

---
+++ Chronos.Server/Controllers/GetDegreeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Chronos.Shared.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chronos.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetDegreeController : ControllerBase
+    {
+        [HttpGet]
+        public Degree Get(int degreeID)
+        {
+            using var db = new AppDbContext();
+            Degree degree = db.Degrees.First(i => i.DegreeID == degreeID);
+
+            return degree;
+        }
+    }
+}
