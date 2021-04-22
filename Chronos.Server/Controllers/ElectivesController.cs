9a39a82da462e58c45aa0978f5d00b03537fb1fe
blobbyguy <sam.gillard44@gmail.com>
2021-04-22 20:53:52 +1000

    [CHR-34] Fixed Logic Issue - added .isCore to conditions

--- Chronos.Server/Controllers/ElectivesController.cs
+++ Chronos.Server/Controllers/ElectivesController.cs
@@ -19,17 +19,17 @@ namespace Chronos.Server.Controllers
             //Instance of DB
             using var db = new AppDbContext();
 
-            //Select all courseID where it is NOT a core course for the given Degree ID
+            //Select all courseID where it is a core course for the given Degree ID
             IEnumerable<int> NotCoreCourseIDs = db.CoreCourses.Where(i => i.DegreeID != DegreeID).Select(i => i.CourseID);
-            //Select all courses where it is NOT a  major core course for the given Major ID
-            IEnumerable<int> NotMajorCourseIDs = db.MajorCourses.Where(i => i.MajorID != MajorID).Select(i => i.CourseID);
+            //Select all courses where it is a  major core course for the given Major ID
+            IEnumerable<int> NotMajorCourseIDs = db.MajorCourses.Where(i => i.IsCore == true && i.MajorID == MajorID).Select(i => i.CourseID);
 
-
-            //Return all courses that are in either of the lists.
+            
+            //Return all courses that are NOT in either of the lists.
             return
                 from course in db.Courses
-                join corecourseID in NotCoreCourseIDs on course.CourseID equals corecourseID
-                join majorcourseID in NotMajorCourseIDs on course.CourseID equals majorcourseID
+                join corecourseID in NotCoreCourseIDs on course.CourseID !equals corecourseID
+                join majorcourseID in NotMajorCourseIDs on course.CourseID !equals majorcourseID
                 select course;
         }
     }
