bcc6fc2ec42075c0bd82d94aae963094c0b541c2
TheCurlyDude <jdawg093@gmail.com>
2021-04-23 23:06:26 +1000

    [CHR-28] Fixed all the problems that it had, changed it to be the right file name.

--- Chronos.Server/Controllers/DegreeController.cs
+++ Chronos.Server/Controllers/DegreeController.cs
@@ -3,23 +3,21 @@ using Chronos.Shared.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Chronos.Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class GetDegreeController : ControllerBase
+    public class DegreeController : ControllerBase
     {
         [HttpGet]
         public Degree Get(int degreeID)
         {
             using var db = new AppDbContext();
-            Degree degree = db.Degrees.Find(i => i.DegreeID == degreeID);
+            Degree Degree = db.Degrees.Find(degreeID);
 
-            return degree;
+            return Degree;
         }
     }
 }
