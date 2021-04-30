d2ad45b138b69fca4f280d7d6b40dbb6c4f404af
TheCosmicNomad <aidenfuller314@gmail.com>
2021-04-30 14:39:05 +1000

    [CHR-56] Added connection string to appsettings.json

--- Chronos/Startup.cs
+++ Chronos/Startup.cs
@@ -32,7 +32,8 @@ namespace Chronos
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(@"Data Source=BEAST;Initial Catalog=Chronos;Integrated Security=True"));
+
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetSection("ConnectionStrings").GetSection("ChronosConnection").Value));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<AppDbContext>();
             services.AddRazorPages();
