f3c589c2354416b94dfc8e75c6b04c03423a7415
TheCosmicNomad <aidenfuller314@gmail.com>
2021-07-06 14:37:12 +1000

    [CHR-89] Added CourseRuntime enum and corrected in Course table

---
+++ Chronos/Models/Enums/CourseRuntime.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chronos.Models.Enums
+{
+    public enum CourseRuntime
+    {
+        Semester1   = 0b_0000_0001,
+        Semester2   = 0b_0000_0001,
+        SummerCS    = 0b_0000_0001,
+        SummerLS    = 0b_0000_0001,
+        Winter      = 0b_0000_0001,
+        Trimester1  = 0b_0000_0001,  
+        Trimester2  = 0b_0000_0001,
+        Trimester3  = 0b_0000_0001
+    }
+}
