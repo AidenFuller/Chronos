3bc8d3354cd5863705215eec3cc99a5c154307dd
TheCurlyDude <jdawg093@gmail.com>
2021-07-09 21:18:56 +1000

    [CHR-109] Added Migrations

---
+++ Chronos/Migrations/20210709111145_DegreeElective.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Chronos.Migrations
+{
+    public partial class DegreeElective : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ElectiveCount",
+                table: "Degrees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ElectiveCount",
+                table: "Degrees");
+        }
+    }
+}
