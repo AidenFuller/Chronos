f555e440d781b0ac26b5a368285546127634bd3d
TheCurlyDude <jdawg093@gmail.com>
2021-07-16 15:13:04 +1000

    [CHR-143] Updated the Migrations.

---
+++ Chronos/Migrations/20210716051215_MajorCourseService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Chronos.Migrations
+{
+    public partial class MajorCourseService : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CourseRequisite",
+                table: "PrerequisiteCourses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CourseRequisite",
+                table: "PrerequisiteCourses");
+        }
+    }
+}
