6af420d15cc7d9284c6569934adc7372fd4ef261
TheCosmicNomad <aidenfuller314@gmail.com>
2021-07-22 12:39:03 +1000

    [CHR-164] Improved Course Availability functionality with separate table.
    Also removed Administrators table since it is now unused

---
+++ Chronos/Models/CourseAvailability.cs
@@ -0,0 +1,18 @@
+﻿using Chronos.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chronos.Models
+{
+    public class CourseAvailability
+    {
+        [Key]
+        public int CourseAvailabilityID { get; set; }
+        public int CourseID { get; set; }
+        public AvailableCampus Campus { get; set; }
+        public CourseRuntime Runtime { get; set; }
+    }
+}
