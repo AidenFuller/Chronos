261a8a55bba99927325679f6c5cecbb9707b5ea1
TheCosmicNomad <aidenfuller314@gmail.com>
2021-07-23 12:45:32 +1000

    [CHR-147] Added JSON Serialisation/Deserialisation functionality

---
+++ Chronos/Shared/Wrappers/SaveState.cs
@@ -0,0 +1,20 @@
+﻿using Chronos.Models;
+using Chronos.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chronos.Shared.Wrappers
+{
+    public class SaveState
+    {
+        public List<List<TileData>> CourseData { get; set; }
+        public Degree Degree;
+        public Major Major;
+        public int BlocksPerYear { get; set; } = 2;
+        public int UnitsPerBlock { get; set; } = 40;
+        public List<Course> CompletedCourses { get; set; }
+        public AvailableCampus Campus { get; set; }
+    }
+}
