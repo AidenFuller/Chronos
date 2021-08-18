05246b815acc3125b5edbf557ee16637c808e3cd
TheCosmicNomad <aidenfuller314@gmail.com>
2021-08-18 21:16:05 +1000

    [CHR-213] new migration

---
+++ Chronos/Migrations/20210818110055_ElectiveUnits.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Chronos.Migrations
+{
+    public partial class ElectiveUnits : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ElectiveCount",
+                table: "Degrees",
+                newName: "ElectiveUnits");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ElectiveUnits",
+                table: "Degrees",
+                newName: "ElectiveCount");
+        }
+    }
+}
